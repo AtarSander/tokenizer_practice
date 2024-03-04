4416e25729b2f5f76fbb2fd5b30c8716774f0111
atarsander <aleksander.szymczyk.stud@pw.edu.pl>
2024-03-04 13:52:46 +0100

    Implemented encoding and decoding functions

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,6 @@
+import unicodedata
+
+
 class BasicTokenizer:
     def train(self, text, vocab_size, verbose=False):
         merges_num = vocab_size - 256
@@ -24,7 +27,7 @@ class BasicTokenizer:
     def update_tokenizer_dicts(self, most_occuring_pair, token_value):
         self.merges[most_occuring_pair] = token_value
         self.vocab[token_value] = self.vocab[most_occuring_pair[0]] + self.vocab[most_occuring_pair[1]]
-        
+
     def merge(self, text, new_pair, new_pair_id):
         new_text = []
         i = 0
@@ -42,3 +45,23 @@ class BasicTokenizer:
         for pair in zip(text, text[1:]):
             counts[pair] = counts.get(pair, 0) + 1
         return counts
+    
+    def encode(self, text):
+        ids = self.encode_to_utf8_list(text)
+        while len(ids) >= 2:
+            pair = self.lowest_merge_index(ids)
+            if pair not in self.merges:
+                break 
+            idx = self.merges[pair]
+            ids = self.merge(ids, pair, idx)
+        return ids
+    
+    def lowest_merge_index(self, ids):
+        stats = self.get_stats(ids)
+        pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
+        return pair
+
+    def decode (self, ids):
+        text_bytes = b"".join(self.vocab[token_value] for token_value in ids)
+        text = text_bytes.decode("utf-8", errors="replace")
+        return text
